267fcee09709c8b06c690b66fea17f6e0ffebe0e
Surendhar Sundaram <surendhar.sundaram@cognizant.com>
2019-11-22 12:01:17 +0530

    initial commit

---
+++ Start Microsite.bat
@@ -0,0 +1,14 @@
+:: Open Visual Studio
+cd C:\Users\768712\Desktop\Micelinious work\Bat files
+call startVisualStudio.bat
+
+:: Start node server
+cd C:\Users\768712\Desktop\Micelinious work\Bat files
+start startNodeServer.cmd
+
+:: Change directory
+cd C:\Users\768712\Desktop\Projects\RCGTHCDE_Micro-Site\FrontEnd
+
+:: Start front end server
+ng serve
+
